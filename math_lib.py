c02fa7a44ae8ab5cf82f2df8f7cfafb5bcc80f69
adziulko <adamdziulko@gmail.com>
2019-09-25 20:46:18 -0600

    first commit of math_lib + test

--- math_lib.py
+++ math_lib.py
@@ -1,6 +1,59 @@
+import sys
+import math
+
 
 def list_mean(L):
-    return None
+    '''Compute the mean of an array. Expects a non-empty array.
+
+    Parameters
+    ----------
+    L : list of int
+        array containing numbers whose mean is desired.
+
+    Returns
+    -------
+    m
+        Arithmetic mean of the values in V
+
+    '''
+    if L is None:
+        return None
+    if len(L) == 0:
+        return None
+
+    s = 0
+
+    for line in L:
+        s += line
+
+
+
+    m = sum(L)/len(L)
+    return m
+
+
+
 
 def list_stdev(L):
-    return None
+    '''Compute the standard deviation of an array. Expects a non-empty array.
+
+    Parameters
+    ----------
+    L : list of int
+        Non-empty array containing numbers whose standard deviation is desired.
+
+    Returns
+    -------
+    sd
+        Standard deviation of the values in V
+
+'''
+    if L is None:
+        return None
+    if len(L) == 0:
+        return None
+
+    
+
+    sd = math.sqrt(sum([(mean(L)-x)**2 for x in L]) / len(L))
+    return sd
